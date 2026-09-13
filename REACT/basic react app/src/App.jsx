c4bd94f1355c50615cc5cc72fdad20692e5ed03c
mujawarswaliya483-sys <mujawarswaliya483@gmail.com>
2026-09-13 18:12:38 +0530

    started react

---
+++ REACT/basic react app/src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import Title from "./Title";
+
+function Title(){
+  return(
+    <h1>Hello World</h1>
+  );
+}
+function App() {
+  return (
+  <div>
+   
+     <Title />
+  </div>
+ );
+}
+
+export default App
